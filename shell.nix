f6f1adc7255f749dff8903dfdaa2454f8887e0c8
Kaushik Chakraborty <git@kaushikc.org>
2018-05-26 16:30:10 +0530

    nix integration & new blog

---
+++ shell.nix
@@ -0,0 +1,36 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default", doBenchmark ? false }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, aeson, base, bytestring, containers, hakyll
+      , lens, lens-aeson, mtl, regex-pcre, stdenv, text, time
+      , time-locale-compat, vector
+      }:
+      mkDerivation {
+        pname = "blog-hakyll";
+        version = "0.1.0.0";
+        src = ./.;
+        isLibrary = false;
+        isExecutable = true;
+        executableHaskellDepends = [
+          aeson base bytestring containers hakyll lens lens-aeson mtl
+          regex-pcre text time time-locale-compat vector
+        ];
+        executableSystemDepends = [pkgs.darwin.apple_sdk.frameworks.Cocoa];
+        license = stdenv.lib.licenses.unfree;
+        hydraPlatforms = stdenv.lib.platforms.none;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  variant = if doBenchmark then pkgs.haskell.lib.doBenchmark else pkgs.lib.id;
+
+  drv = variant (haskellPackages.callPackage f {});
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
