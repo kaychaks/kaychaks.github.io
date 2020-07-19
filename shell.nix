493fe0987472005f3765c3ee6c2103e23c063292
Kaushik Chakraborty <git@kaushikc.org>
2020-07-19 12:11:23 +0530

    nixos to rescue

--- shell.nix
+++ shell.nix
@@ -1,8 +1,6 @@
 {
-  # bootstrap ? import <nixpkgs> {},
-  compiler ? "ghc883",
-  doBenchmark ? false
-}:
+# bootstrap ? import <nixpkgs> {},
+compiler ? "ghc883", doBenchmark ? false }:
 
 let
 
@@ -20,17 +18,20 @@ let
   # pkgs = import versions.nixpkgs {};
 
   sources = import ./nix/sources.nix;
-  haskellNix = import sources."haskell.nix" {};
+  haskellNix = import sources."haskell.nix" { };
   nixpkgsSrc = haskellNix.sources.nixpkgs-2003;
   # nixpkgs = import sources.nixpkgs haskellNix.nixpkgsArgs;
   nixpkgs = import nixpkgsSrc haskellNix.nixpkgsArgs;
   haskell = nixpkgs.haskell-nix;
 
-  hspkgs = haskell.cabalProject {
-    src = haskell.haskellLib.cleanGit { src = ./.; };
+  project = haskell.cabalProject {
+    src = haskell.haskellLib.cleanGit {
+      name = "blog-hakyll";
+      src = ./.;
+    };
     compiler-nix-name = compiler;
     configureArgs = "";
-    modules = [];
+    modules = [ ];
     index-state = "2020-07-05T00:00:00Z";
   };
 
@@ -40,10 +41,12 @@ let
     src = nixpkgs.lib.cleanSource ./public;
     LANG = "en_US.UTF-8";
 
-    buildInputs = [ hspkgs.blog-hakyll.components.exes.site ];
+    buildInputs = [ project.blog-hakyll.components.exes.site ];
 
     preConfigure = ''
-    export LANG="en_US.UTF-8";
+      export LOCALE_ARCHIVE="${nixpkgs.glibcLocales}/lib/locale/locale-archive"
+      export LANG="en_US.UTF-8"
+      export LC_CTYPE="en_US.UTF-8"
     '';
 
     buildPhase = ''
@@ -55,46 +58,49 @@ let
     '';
   };
 
-  siteShell = hspkgs.shellFor {
+  siteShell = project.shellFor {
     packages = ps: [ ps.blog-hakyll ];
     withHoogle = true;
-    tools = { cabal = "3.2.0.0"; hlint = "3.1.6"; };
+    tools = {
+      cabal = "3.2.0.0";
+      hlint = "3.1.6";
+    };
     buildInputs = with nixpkgs.haskellPackages; [ ghcid ];
     exactDeps = true;
   };
 
-in
+in if nixpkgs.lib.inNixShell then
+  siteShell
+else
+  site
 
-  if nixpkgs.lib.inNixShell then siteShell else site
+  #   f = { mkDerivation, aeson, base, bytestring, containers, hakyll
+  #       , lens, lens-aeson, mtl, regex-pcre, stdenv, text, time
+  #       , time-locale-compat, vector
+  #       }:
+  #       mkDerivation {
+  #         pname = "blog-hakyll";
+  #         version = "0.1.0.0";
+  #         src = ./.;
+  #         isLibrary = false;
+  #         isExecutable = true;
+  #         executableHaskellDepends = [
+  #           aeson base bytestring containers hakyll lens lens-aeson mtl
+  #           regex-pcre text time time-locale-compat vector
+  #         ];
+  #         executableSystemDepends = if pkgs.stdenv.isDarwin then [ pkgs.darwin.apple_sdk.frameworks.Cocoa pkgs.cabal-install ] else [ pkgs.cabal-install ];
+  #         license = stdenv.lib.licenses.unfree;
+  #         hydraPlatforms = stdenv.lib.platforms.none;
+  #       };
 
+  #   haskellPackages = if compiler == "default"
+  #                        then pkgs.haskellPackages
+  #                        else pkgs.haskell.packages.${compiler};
 
-#   f = { mkDerivation, aeson, base, bytestring, containers, hakyll
-#       , lens, lens-aeson, mtl, regex-pcre, stdenv, text, time
-#       , time-locale-compat, vector
-#       }:
-#       mkDerivation {
-#         pname = "blog-hakyll";
-#         version = "0.1.0.0";
-#         src = ./.;
-#         isLibrary = false;
-#         isExecutable = true;
-#         executableHaskellDepends = [
-#           aeson base bytestring containers hakyll lens lens-aeson mtl
-#           regex-pcre text time time-locale-compat vector
-#         ];
-#         executableSystemDepends = if pkgs.stdenv.isDarwin then [ pkgs.darwin.apple_sdk.frameworks.Cocoa pkgs.cabal-install ] else [ pkgs.cabal-install ];
-#         license = stdenv.lib.licenses.unfree;
-#         hydraPlatforms = stdenv.lib.platforms.none;
-#       };
+  #   variant = if doBenchmark then pkgs.haskell.lib.doBenchmark else pkgs.lib.id;
 
-#   haskellPackages = if compiler == "default"
-#                        then pkgs.haskellPackages
-#                        else pkgs.haskell.packages.${compiler};
+  #   drv = variant (haskellPackages.callPackage f {});
 
-#   variant = if doBenchmark then pkgs.haskell.lib.doBenchmark else pkgs.lib.id;
+  # in
 
-#   drv = variant (haskellPackages.callPackage f {});
-
-# in
-
-#   if pkgs.lib.inNixShell then drv.env else drv
+  #   if pkgs.lib.inNixShell then drv.env else drv
