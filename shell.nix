2920b000409354859a91374c5bd82e150357bb7e
Kaushik Chakraborty <git@kaushikc.org>
2020-11-05 17:51:13 +0530

    updated configs

--- shell.nix
+++ shell.nix
@@ -33,7 +33,7 @@ let
     configureArgs = "";
     modules = [ ];
     index-state = "2020-07-05T00:00:00Z";
-    plan-sha256 = "1v71yj9zwcblcvk55zpzrb11nixp0hkyq0rb1pnz4c5878v4aygd";
+    # plan-sha256 = "1v71yj9zwcblcvk55zpzrb11nixp0hkyq0rb1pnz4c5878v4aygd";
     # materialized = ./blog-hakyll.mat;
     # checkMaterialization = true;
   };
