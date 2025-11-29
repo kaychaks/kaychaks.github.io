6c8e16817873761503e0d17dbfa28e6f924243b5
Kaushik Chakraborty <git@kaushikc.org>
2025-11-29 06:38:15 +0530

    chore:devenv setup

---
+++ devenv.nix
@@ -0,0 +1,11 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  languages.javascript.enable = true;
+  languages.javascript.pnpm.enable = true;
+
+  languages.haskell.enable = true;
+  languages.haskell.cabal.enable = true;
+  
+  packages = [ pkgs.pcre ];
+}
